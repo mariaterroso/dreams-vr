25040b1e1f17f649a32011d9a0f4f73e65423bc3
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2022-04-27 00:44:42 +0100

    Initial work

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Color targetColorEnter;
+    public Color targetColorExit;
+    public Color targetColorClick;
+
+    public void ChangeColorEnter()
+    {
+        GetComponent<Renderer>().material.color = targetColorEnter;
+    }
+
+    public void ChangeColorExit()
+    {
+        GetComponent<Renderer>().material.color = targetColorExit;
+    }
+
+    public void ChangeColorClick()
+    {
+        GetComponent<Renderer>().material.color = targetColorClick;
+    }
+
+}
