abe94fad6384df1971736858bbfbb2287ea3697e
mariaterroso <79975186+mariaterroso@users.noreply.github.com>
2022-05-15 19:34:16 +0100

    Added trees and Main Menu w/scripts working

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        UnityEngine.Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void FirstMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
