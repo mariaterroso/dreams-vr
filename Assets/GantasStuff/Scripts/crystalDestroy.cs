313e7570380c8ca75f538e925822c8fce610c3a5
Gantas <gantasgan@gmail.com>
2022-05-24 14:52:31 +0100

    Score System

--- Assets/GantasStuff/Scripts/crystalDestroy.cs
+++ Assets/GantasStuff/Scripts/crystalDestroy.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class crystalDestroy : MonoBehaviour
 {
+
+    int score;
+    public TextMeshProUGUI scoreText;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        score = 0;
     }
 
     // Update is called once per frame
@@ -19,9 +27,10 @@ public class crystalDestroy : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == 8) 
-        
+        if (collision.gameObject.layer == 8)         
         {
+            score++;
+            scoreText.text = score.ToString();
             Destroy(collision.gameObject);
         }
 
