fe2c49b98a517f6eca905292b3ef10b2847598db
Gantas <gantasgan@gmail.com>
2022-05-24 15:29:01 +0100

    Sound/Mouth

---
+++ Assets/GantasStuff/Scripts/Eating.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Eating : MonoBehaviour
+{
+
+    public AudioSource source;
+    public AudioClip clip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 9)         
+        {
+            source.PlayOneShot(clip);
+            Destroy(collision.gameObject);
+        }
+
+    }
+
+}
